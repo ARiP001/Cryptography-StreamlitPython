fa63e39513d24d682ac82aabb235832e87f74207
ARiP001 <142637487+ARiP001@users.noreply.github.com>
2024-11-18 20:44:57 +0700

    Update database.py

--- database.py
+++ database.py
@@ -9,33 +9,23 @@ db_name = st.secrets["general"]["DB_NAME"]
 db_user = st.secrets["general"]["DB_USER"]
 db_password = st.secrets["general"]["DB_PASSWORD"]
 
-# Establish a connection
-try:
-    # Connect to the Supabase PostgreSQL database
-    connection = psycopg2.connect(
+def get_connection():
+    return psycopg2.connect(
         host=db_host,
         port=db_port,
         dbname=db_name,
         user=db_user,
         password=db_password
     )
-    cursor = connection.cursor()
-    cursor.execute("SELECT 'Connection successful!'")
-    result = cursor.fetchone()
-    st.success(result[0])  # Display success message in Streamlit
-    cursor.close()
-    connection.close()
-except Exception as e:
-    st.error(f"Database connection failed: {e}")
 
 def check_login(username, password):
     conn = get_connection()
-    cursor = conn.cursor(dictionary=True)
+    cursor = conn.cursor()
     cursor.execute("SELECT password_hash FROM users WHERE username = %s", (username,))
     user = cursor.fetchone()
     conn.close()
 
-    if user and bcrypt.checkpw(password.encode(), user['password_hash'].encode()):
+    if user and bcrypt.checkpw(password.encode(), user[0].encode()):
         return True
     return False
 
